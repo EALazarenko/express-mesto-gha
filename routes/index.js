9d69b7041b5641eb150a54524396a6709da4cf10
Ekaterina Lazarenko <lazarenkoea@yandex.ru>
2023-04-24 19:28:50 +0300

    404 test

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,17 @@ const router = require('express').Router();
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 const {
-  handleErrors,
-  NotFoundError,
+  /* handleErrors, */
+  /* NotFoundError, */
+  HTTP_STATUS_NOT_FOUND
 } = require('../utils/handleErrors');
 
 router
   .use('/users', userRoutes)
   .use('/cards', cardRoutes)
   .use('*', (req, res) => {
-    const newError = new NotFoundError(); // скобки
-    handleErrors(newError, res);
+    /* const newError = new NotFoundError(); // скобки
+    handleErrors(newError, res); */
+    res.status(HTTP_STATUS_NOT_FOUND).send({ message: "Страница не найдена" });
   });
 module.exports = router;
